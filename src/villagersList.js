db1b7524e8314dfc66843510f7881808201f68b3
Bailey Dalton <baidalton@gmail.com>
2020-09-14 14:50:20 -0600

    button css

--- src/villagersList.js
+++ src/villagersList.js
@@ -33,45 +33,29 @@ class VillagersFilter extends Component {
         duck: villagers.filter(villager => villager.species === 'Duck'),
         ostrich: villagers.filter(villager => villager.species === 'Ostrich'),
         rabbit: villagers.filter(villager => villager.species === 'Rabbit'),
-        gorilla: villagers.filter(villager => villager.species === 'Gorilla')
+        gorilla: villagers.filter(villager => villager.species === 'Gorilla'),
+        showSheep: false,
+        showBirds: false,
+        showSquirrels: false
     }
 
+
     sheepSortHandler = () => {
-       const newSheep = this.state.sheep.map((shee, index) => {
-           return {
-               imagePath: shee.image_url,
-               birthDay: shee.birthday_day,
-               birthMonth: shee.birthday_month,
-               sheepName: shee.name,
-               sheepSign: shee.sign,
-               sheepPersonality: shee.personality
-           }
-        })
-
-        this.setState({
-            sheep: newSheep
-        })
-
-        console.log(newSheep)
+        const doesShow = this.state.showSheep;
+        this.setState({showSheep: !doesShow})
 
     }
 
     birdSortHandler = () => {
-        const newBirds = this.state.bird.map((bird, index) => {
-            return {
-                imagePath: bird.image_url,
-                birthDay: bird.birthday_day,
-                birthMonth: bird.birthday_month,
-                birdName: bird.name,
-                birdSign: bird.sign,
-                birdPersonality: bird.personality
-            }
-        })
-
-        this.setState({
-            bird: newBirds
-        })
+        const doesShow = this.state.showBirds;
+        this.setState({showBirds: !doesShow})
     }
+
+    squirrelSortHandler = () => {
+        const doesShow = this.state.showSquirrels;
+        this.setState({showSquirrels: !doesShow})
+    }
+
 
 
     
@@ -80,40 +64,71 @@ class VillagersFilter extends Component {
             <div className="species">
                 <button onClick={this.sheepSortHandler}>Sheep</button>
                 <button onClick={this.birdSortHandler}>Birds</button>
-
-                <div className="list">
+                <button onClick={this.squirrelSortHandler}>Squirrels</button>
+                { this.state.showSheep === true ? 
+                <div>
+                    <h1>Sheep</h1>
+                    <div className="list">
                     {
                          this.state.sheep.map((shee, index) => {
                             return (
                                 <div className="listItem">
-                                    <img alt="" src={shee.imagePath}></img>
-                                    <p>{shee.sheepName}</p>
-                                    <p>{shee.sheepPersonality}</p>
-                                    <p>{shee.birthDay} {shee.birthMonth}</p>
-                                    <p>{shee.sheepSign}</p>
+                                    <img alt="" src={shee.image_url}></img>
+                                    <p>{shee.name}</p>
+                                    <p>Personality: {shee.personality}</p>
+                                    <p>Birthday: {shee.birthday_day} {shee.birthday_month}</p>
+                                    <p>Sign: {shee.sign}</p>
                                 </div>
                             )
                          })
-                    }
-                </div>
-                <div className="list">
+                        }
+                        </div>
+                </div> : null
+                }
+                {this.state.showBirds === true ?
+                <div>
+                    <h1>Birds</h1>
+                    <div className="list">
                     {
                          this.state.bird.map((bird, index) => {
                             return (
                                 <div className="listItem">
-                                    <img alt="" src={bird.imagePath}></img>
-                                    <p>{bird.birdName}</p>
-                                    <p>{bird.birdPersonality}</p>
-                                    <p>{bird.birthDay} {bird.birthMonth}</p>
-                                    <p>{bird.birdSign}</p>
+                                    <img alt="" src={bird.image_url}></img>
+                                    <p>{bird.name}</p>
+                                    <p>Personality: {bird.personality}</p>
+                                    <p>Name: {bird.birthday_day} {bird.birthday_month}</p>
+                                    <p>Sign: {bird.sign}</p>
                                 </div>
                             )
                          })
                     }
-                </div>
+                    </div>
+                </div> : null
+                } 
+                {this.state.showSquirrels === true ?
+                <div>
+                    <h1>Squirrels</h1>
+                    <div className="list">
+                    {
+                         this.state.squirrel.map((squirrel, index) => {
+                            return (
+                                <div className="listItem">
+                                    <img src={squirrel.image_url}></img>
+                                    <p>{squirrel.name}</p>
+                                    <p>Personality: {squirrel.personality}</p>
+                                    <p>Birthday: {squirrel.birthday_day} {squirrel.birthday_month}</p>
+                                    <p>Sign: {squirrel.sign}</p>
+                                </div>
+                            )
+                         })
+                    }
+                    </div>
+                </div> : null
+                } 
             </div>
         )
     }
 
 }
+
 export default VillagersFilter
