fa1ac7352e13c503b2d9d916d92ccf3a96196280
Bailey Dalton <baidalton@gmail.com>
2020-10-23 21:40:07 -0600

    more styling

--- src/components/bugs.js
+++ src/components/bugs.js
@@ -2,33 +2,43 @@ import React from "react";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import QueryBuilderIcon from "@material-ui/icons/QueryBuilder";
-import { useAnmialCrossingContext } from "../contexts/AnimalCrossingContext"
+import { useAnmialCrossingContext } from "../contexts/AnimalCrossingContext";
 
 const Bugs = () => {
-  const bugsData = useAnmialCrossingContext()
+  const bugsData = useAnmialCrossingContext();
 
+  const allBugs = bugsData.bugs.map((bug) => {
+    return {
+      id: bug.id,
+      img: bug.icon_uri,
+      name: bug.name["name-USen"],
+      price: bug.price,
+      time: bug.availability.time === "" ? "All Day" : bug.availability.time,
+      location: bug.availability.location,
+    }
+  })
 
   return (
     <div className="bugs">
       <h1> {bugsData.bugs.length} Bugs</h1>
       <div className="list">
-        {bugsData.bugs.map((bug) => {
+        {allBugs.map((bug) => {
           return (
             <div key={bug.id} className="listItem">
               <div>
-                <img alt={bug.name["name-USen"]} src={bug.icon_uri} />
-                <p>{bug.name["name-USen"]}</p>
+                <img alt={bug.name} src={bug.img} />
+                <p>{bug.name}</p>
                 <p>
                   <AttachMoneyIcon /> {bug.price}
                 </p>
               </div>
-              <div>
-                <p>
-                  <LocationOnIcon /> {bug.availability.location}
-                </p>
-                <p>
-                  <QueryBuilderIcon /> {bug.availability.time}
-                </p>
+              <div className="attributes">
+                <div className="attribute">
+                  <LocationOnIcon /> <p>{bug.location}</p>
+                </div>
+                <div className="attribute">
+                  <QueryBuilderIcon /> <p>{bug.time}</p>
+                </div>
               </div>
             </div>
           );
--- src/components/fish.js
+++ src/components/fish.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
-import { useAnmialCrossingContext } from "../contexts/AnimalCrossingContext"
-
+import React from "react";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import QueryBuilderIcon from "@material-ui/icons/QueryBuilder";
+import { useAnmialCrossingContext } from "../contexts/AnimalCrossingContext";
 
 const Fish = () => {
-    const fishesData = useAnmialCrossingContext()
+  const fishesData = useAnmialCrossingContext();
 
-    const allFish = fishesData.fishes.map((fish) => {
-        return {
-            id: fish.id,
-            img: fish.icon_uri,
-            name: fish.name["name-USen"],
-            price: fish.price,
-            time: fish.availability.time === "" ? "All Day" : fish.availability.time,
-            location: fish.availability.location
-        }
+  const allFish = fishesData.fishes.map((fish) => {
+    return {
+      id: fish.id,
+      img: fish.icon_uri,
+      name: fish.name["name-USen"],
+      price: fish.price,
+      time: fish.availability.time === "" ? "All Day" : fish.availability.time,
+      location: fish.availability.location,
+    };
+  });
 
-    })
-
-    return (
-        <div className="fish">
-            <h1>{fishesData.fishes.length} Fish</h1>
-            <div className="list">
-                {allFish.map((fish) => {
-                    return (
-                        <div key={fish.id} className="listItem">
-                        <div>
-                            <img alt="" src={fish.img}/>
-                            <p>{fish.name}</p>
-                            <p><AttachMoneyIcon />{fish.price}</p>
-                        </div>
-                        <div>
-                        <p><LocationOnIcon />{fish.location}</p>
-                        <p><QueryBuilderIcon />{fish.time} </p>
-
-                        </div>
-                        </div>
-                    )
-                })}
+  return (
+    <div className="fish">
+      <h1>{fishesData.fishes.length} Fish</h1>
+      <div className="list">
+        {allFish.map((fish) => {
+          return (
+            <div key={fish.id} className="listItem">
+              <div>
+                <img alt="" src={fish.img} />
+                <p>{fish.name}</p>
+                <p>
+                  <AttachMoneyIcon />
+                  {fish.price}
+                </p>
+              </div>
+              <div className="attributes">
+                <div className="attribute">
+                  <LocationOnIcon /> <p>{fish.location}</p>
+                </div>
+                <div className="attribute">
+                  <QueryBuilderIcon />
+                  <p>{fish.time} </p>
+                </div>
+              </div>
             </div>
-        </div>
-    )
-
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Fish;
