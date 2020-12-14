3e2d2a7daa5568e9be834a51cfefcaebaeeba4a9
Bailey Dalton <baidalton@gmail.com>
2020-12-14 15:50:45 -0700

    login working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from "react";
-// import React from "react"
-import { Button } from "@material-ui/core";
+// import React, { useState, useContext } from "react";
+import React from "react"
+// import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import { FaFish } from "react-icons/fa";
@@ -9,7 +9,7 @@ import { FaPaw } from 'react-icons/fa';
 import { GiSadCrab } from 'react-icons/gi';
 import { BsFillPersonFill } from 'react-icons/bs'
 import Login from "./Login";
-import { AuthContext } from '../contexts/AuthContext'
+// import { AuthContext } from '../contexts/AuthContext'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -20,56 +20,57 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-export default function ButtonAppBar() {
+const Home = () => {
   const classes = useStyles()
-  const [loginOpen, setLoginOpen] = useState(false)
 
-  const authContext = useContext(AuthContext)
+  // export default function ButtonAppBar() {
+  //   const classes = useStyles()
+  //   const [loginOpen, setLoginOpen] = useState(false)
 
-  const handleAuth = () => {
-    if (authContext.isAuthenticated) {
-      authContext.logout()
-      setLoginOpen(false)
-      return
-    }
-    if (!authContext.isAuthenticated) {
-      if (!loginOpen) {
-        setLoginOpen(true)
-        return
-      }
-      setLoginOpen(false)
-    }
-  }
+  //   const authContext = useContext(AuthContext)
+
+  //   const handleAuth = () => {
+  //     if (authContext.isAuthenticated) {
+  //       authContext.logout()
+  //       setLoginOpen(false)
+  //       return
+  //     }
+  //     if (!authContext.isAuthenticated) {
+  //       if (!loginOpen) {
+  //         setLoginOpen(true)
+  //         return
+  //       }
+  //       setLoginOpen(false)
+  //     }
+  //   }
 
   return (
     <div className={classes.root}>
-        <div className="homeMenu">
-          <NavLink to="/fish" className="menuItem">
-            <FaFish className="icon"/>
-            <p>FISH</p>
-          </NavLink>
-          <NavLink to="/bugs" className="menuItem">
-            <IoIosBug className="icon"/>
-            <p>BUGS</p>
-          </NavLink>
-          <NavLink to="/seacreatures" className="menuItem">
-            <GiSadCrab className="icon"/>
-            <p>SEA CREATURES</p>
-          </NavLink>
-          <NavLink to="/villagers" className="menuItem">
-            <FaPaw className="icon"/>
-            <p>VILLAGERS</p>
-          </NavLink>
-          <div className="menuItem">
-          <BsFillPersonFill className="icon"/>       
-            {
-              authContext.isAuthenticated ? <Button color='inherit' onClick={handleAuth}>Logout</Button> :
-              <Button color='inherit' onClick={handleAuth}>Login</Button>
-            }
-            </div>
-        </div>
-        <Login open={loginOpen} onClose={handleAuth}/>
+    <div className="homeMenu">
+      <NavLink to="/fish" className="menuItem">
+        <FaFish className="icon"/>
+        <p>FISH</p>
+      </NavLink>
+      <NavLink to="/bugs" className="menuItem">
+        <IoIosBug className="icon"/>
+        <p>BUGS</p>
+      </NavLink>
+      <NavLink to="/seacreatures" className="menuItem">
+        <GiSadCrab className="icon"/>
+        <p>SEA CREATURES</p>
+      </NavLink>
+      <NavLink to="/villagers" className="menuItem">
+        <FaPaw className="icon"/>
+        <p>VILLAGERS</p>
+      </NavLink>
+      <div className="menuItem signUp">   
+      <BsFillPersonFill className="icon"/>       
+      <Login />
+      </div>
+
     </div>
+</div>
   )
 }
 
+export default Home
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,190 @@
-import React, { useContext } from "react";
-import { Dialog,  Box, TextField, Button, makeStyles } from "@material-ui/core";
+// // import React, { useContext } from "react";
+// import React from "react"
+// import { Dialog,  Box, TextField, Button, makeStyles } from "@material-ui/core";
+// import { Formik } from "formik";
+// import * as Yup from "yup";
+// // import { AuthContext } from "../contexts/AuthContext";
+
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     color: "#fff",
+//     paddingLeft: 40,
+//     paddingRight: 40,
+//     paddingTop: 10,
+//     paddingBottom: 10,
+//     borderRadius: 10,
+//     fontSize: 16,
+//   },
+
+//   providerIcon: {
+//     height: 40,
+//   },
+
+//   form: {
+//     margin: 15,
+//   }
+// }));
+
+// const Login = (props) => {
+//   const classes = useStyles();
+//   const { open, onClose } = props;
+
+//   // const authContext = useContext(AuthContext);
+//   // const { signInWithGoogle, signInWithEmailAndPassword } = authContext;
+
+//   // const handleGoogleClick = async () => {
+//   //   try {
+//   //     await signInWithGoogle();
+//   //     handleClose();
+//   //   } catch (error) {
+//   //     console.log(error);
+//   //   }
+//   // };
+
+//   const handleClose = () => {
+//     onClose(false);
+//   };
+
+
+//   return (
+//     <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog">
+//       {/* <Button
+//         className={classes.googleButton}
+//         fullWidth
+//         onClick={handleGoogleClick}
+//         size="large"
+//         variant="contained"
+//       >
+//         <img
+//           alt="Google"
+//           className={classes.providerIcon}
+//           src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
+//         />
+//       </Button> */}
+//         <Formik
+//           initialValues={{
+//             email: "",
+//             password: "",
+//             submit: null,
+//           }}
+//           validationSchema={Yup.object().shape({
+//             email: Yup.string().email().max(50).required(),
+//             password: Yup.string().min(8).max(50).required(),
+//           })}
+//           onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+//             try {
+//               // await signInWithEmailAndPassword(values.email, values.password);
+//               console.log(values.email, values.password)
+//               handleClose()
+//             } catch (err) {
+//               console.log(err);
+//               setStatus({ success: false });
+//               setErrors({ submit: err.message });
+//               setSubmitting(false);
+//             }
+//           }}
+//         >
+//           {({
+//             values,
+//             errors,
+//             touched,
+//             handleChange,
+//             handleBlur,
+//             handleSubmit,
+//             isSubmitting,
+//           }) => (
+//             <form noValidate onSubmit={handleSubmit} autoComplete="off">
+//               <TextField
+//                 autoFocus
+//                 required
+//                 fullWidth
+//                 margin="normal"
+//                 label="Email Address"
+//                 type="email"
+//                 name="email"
+//                 onChange={handleChange}
+//                 onBlur={handleBlur}
+//                 value={values.email}
+//                 error={Boolean(touched.email && errors.email)}
+//                 helperText={touched.email && errors.email}
+//               />
+//               <TextField
+//                 required
+//                 fullWidth
+//                 margin="normal"
+//                 label="Password"
+//                 type="password"
+//                 name="password"
+//                 onChange={handleChange}
+//                 onBlur={handleBlur}
+//                 value={values.password}
+//                 error={Boolean(touched.password && errors.password)}
+//                 helperText={touched.password && errors.password}
+//               />
+//               <TextField
+//                 margin="normal"
+//                 label="Island Name"
+//                 id="name"
+//                 name="name"
+//                 onChange={handleChange}
+//                 onBlur={handleBlur}
+//                 value={values.name}
+//               />
+//               {/* <Hemisphere /> */}
+
+//               <Box>
+//                 <Button
+//                   onClick={handleClose}
+//                   variant="contained"
+//                   color="primary"
+//                   className={classes.form}
+
+//                 >
+//                   Cancel
+//                 </Button>
+//                 <Button
+//                   color="primary"
+//                   variant="contained"
+//                   type="submit"
+//                   disabled={Boolean(errors.email || errors.password)}
+//                 >
+//                   Signup
+//                 </Button>
+//               </Box>
+//             </form>
+//           )}
+//         </Formik>
+//     </Dialog>
+//   );
+// };
+
+// export default Login;
+
+import React, { useState, useContext } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Button,
+  makeStyles
+} from "@material-ui/core";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { AuthContext } from "../contexts/AuthContext";
+import Hemisphere from "./Hemisphere";
 
 const useStyles = makeStyles((theme) => ({
+  // navStyle: {
+  //   color: "#000",
+  //   fontSize: 16,
+  //   textDecoration: "none",
+  //   '&:hover': {
+  //     color: "#000",
+  //   }
+  // },
+
   button: {
     color: "#fff",
     paddingLeft: 40,
@@ -14,146 +194,148 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 10,
     fontSize: 16,
   },
+}))
 
-  providerIcon: {
-    height: 40,
-  },
+const Login = () => {
+  const classes = useStyles()
+  const [open, setOpen] = useState(false);
 
-  form: {
-    margin: 15,
-  }
-}));
-
-const Login = (props) => {
-  const classes = useStyles();
-  const { open, onClose } = props;
-
-  const authContext = useContext(AuthContext);
-  const { signInWithGoogle, signInWithEmailAndPassword } = authContext;
-
-  const handleGoogleClick = async () => {
-    try {
-      await signInWithGoogle();
-      handleClose();
-    } catch (error) {
-      console.log(error);
-    }
+  const handleOpen = () => {
+    setOpen(true);
   };
 
   const handleClose = () => {
-    onClose(false);
+    setOpen(false);
   };
 
+    const authContext = useContext(AuthContext);
 
-  return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog">
-      <Button
-        className={classes.googleButton}
-        fullWidth
-        onClick={handleGoogleClick}
-        size="large"
-        variant="contained"
+  const handleAuth = () => {
+    console.log(authContext.isAuth);
+    if (authContext.isAuth) {
+      authContext.logout();
+      setOpen(false);
+      return;
+    }
+    if (!authContext.isAuth) {
+      if (!open) {
+        setOpen(true);
+        return;
+      }
+      setOpen(false);
+    }
+  };
+
+    return (
+    <div>
+      {authContext.isAuth ? (
+        <Button className={classes.button} onClick={handleAuth}>
+          Logout
+        </Button>
+      ) : (
+        <Button className={classes.button} onClick={handleOpen}>
+          Signup
+        </Button>
+      )}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
       >
-        <img
-          alt="Google"
-          className={classes.providerIcon}
-          src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
-        />
-      </Button>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-            submit: null,
-          }}
-          validationSchema={Yup.object().shape({
-            email: Yup.string().email().max(50).required(),
-            password: Yup.string().min(8).max(50).required(),
-          })}
-          onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-            try {
-              await signInWithEmailAndPassword(values.email, values.password);
-              console.log(values.email, values.password)
-              handleClose()
-            } catch (err) {
-              console.log(err);
-              setStatus({ success: false });
-              setErrors({ submit: err.message });
-              setSubmitting(false);
-            }
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <form noValidate onSubmit={handleSubmit} autoComplete="off">
-              <TextField
-                autoFocus
-                required
-                fullWidth
-                margin="normal"
-                label="Email Address"
-                type="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                error={Boolean(touched.email && errors.email)}
-                helperText={touched.email && errors.email}
-              />
-              <TextField
-                required
-                fullWidth
-                margin="normal"
-                label="Password"
-                type="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-                error={Boolean(touched.password && errors.password)}
-                helperText={touched.password && errors.password}
-              />
-              <TextField
-                margin="normal"
-                label="Island Name"
-                id="name"
-                name="name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.name}
-              />
-              {/* <Hemisphere /> */}
+        <DialogTitle id="form-dialog-title">Sign Up</DialogTitle>
+        <DialogContent>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+              submit: null,
+            }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string().email().max(50).required(),
 
-              <Box>
-                <Button
-                  onClick={handleClose}
-                  variant="contained"
-                  color="primary"
-                  className={classes.form}
+              password: Yup.string().min(8).max(50).required(),
+            })}
+            onSubmit={(values, { setErrors, setStatus, setSubmitting }) => {
+              try {
+                authContext.login();
+                console.log(values.email, values.password, values.name, values.hemisphere);
+                handleClose();
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <form noValidate onSubmit={handleSubmit} autoComplete="off">
+                <TextField
+                  autoFocus
+                  required
+                  fullWidth
+                  margin="normal"
+                  label="Email Address"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  error={Boolean(touched.email && errors.email)}
+                  helperText={touched.email && errors.email}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  margin="normal"
+                  label="Password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  error={Boolean(touched.password && errors.password)}
+                  helperText={touched.password && errors.password}
+                />
+                <TextField
+                  margin="normal"
+                  label="Island Name"
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                />
+                {/* <Hemisphere /> */}
 
-                >
-                  Cancel
-                </Button>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  type="submit"
-                  disabled={Boolean(errors.email || errors.password)}
-                >
-                  Signup
-                </Button>
-              </Box>
-            </form>
-          )}
-        </Formik>
-    </Dialog>
+                <DialogActions>
+                  <Button
+                    onClick={handleClose}
+                    variant="contained"
+                    color="primary"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    disabled={Boolean(errors.email || errors.password)}
+                  >
+                    Signup
+                  </Button>
+                </DialogActions>
+              </form>
+            )}
+          </Formik>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
 
