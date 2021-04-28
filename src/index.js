55671cd77b010193823f0a72c771805920ac4e42
Bailey Dalton <baidalton@gmail.com>
2021-04-28 17:53:30 -0600

    added server connection

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import AuthContextProvider from "./contexts/AuthContext";
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql/',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 })
 
